ebec338074134bfbb9d7a306c7e1ef866b43a969
ccoltekin@gmail.com <ccoltekin@gmail.com>
2015-04-07 12:45:21 +0000

    Redhat/Debian packagin setup.

---
+++ src/assembly/bin/service.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# This is a simple startup script that should work for the
+# dropwizard based web services.
+#
+
+APPNAME=$(basename $0)
+BASEDIR=${BASEDIR:-.}
+CONFFILE=${CONFFILE:-${BASEDIR}/${APPNAME}.yaml}
+HEAPSIZE=${HEAPSIZE:-2048m}
+JARFILE=${JARFILE:-${BASEDIR}/${APPNAME}.jar}
+
+while getopts ":c:h:" opt; do
+    case $opt in
+        c) CONFFILE=${OPTARG}
+        ;;
+        h) HEAPSIZE=${OPTARG}
+        ;;
+    esac
+done
+
+cd ${BASEDIR}
+exec java -Xmx${HEAPSIZE} -jar ${JARFILE} server ${CONFFILE}
