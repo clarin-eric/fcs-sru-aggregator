897290cca9448e61e72153c5157b6949d081fa4a
Emanuel Dima <emanuel.dima@uni-tuebingen.de>
2015-04-30 17:01:10 +0000

    v. beta-46: no more tables in clarinservices module, icon included

--- src/main/resources/assets/clarinservices/clarinservices.js
+++ src/main/resources/assets/clarinservices/clarinservices.js
@@ -1,6 +1,65 @@
 (function() {
 "use strict";
 
+var more_img = "clarinservices/more-clarin-services.png";
+
+var service_list = [
+	{
+		href: 'http://www.clarin.eu/portal',
+		image: 'https://www.clarin.eu/sites/default/files/portal_small.png',
+		title: "CLARIN portal",
+		description: "Quick introduction to CLARIN, giving an impression about what's currently available",
+	},
+	{
+		href: 'http://www.clarin.eu/services/depositing',
+		image: 'http://www.clarin.eu/sites/default/files/long-term-archive.jpg',
+		title: "Depositing services",
+		description: "Store language resources in a sustainable repository at a CLARIN centre",
+	},
+	{
+		href: 'http://www.clarin.eu/vlo',
+		image: 'https://www.clarin.eu/sites/default/files/vlo_small.png',
+		title: "Virtual Language Observatory",
+		description: "Discover language resources using a faceted browser or a map",
+	},
+	{
+		href: 'http://www.clarin.eu/content/easy-access-protected-resources',
+		image: 'http://www.clarin.eu/sites/default/files/federated-identity.jpg',
+		title: "Easy federated access",
+		description: "Get easy access to protected resources, with your institutional username and password.",
+	},
+	{
+		href: 'http://www.clarin.eu/content/web-services',
+		image: 'https://www.clarin.eu/sites/default/files/webservice.png',
+		title: "Web services and applications",
+		description: "Explore and analyze language data with a wide variety of tools",
+	},
+	{
+		href: 'http://www.clarin.eu/content/virtual-collections',
+		image: 'https://www.clarin.eu/sites/default/files/virtualcollection.png',
+		title: "Virtual Collections",
+		description: "Create your own digital bookmarks, ideal for citing data sets.",
+	},
+	{
+		href: 'http://www.clarin.eu/content/language-resource-inventory',
+		image: 'https://www.clarin.eu/sites/default/files/lrt-inventory_small.png',
+		title: "Language Resource Inventory",
+		description: "Submit and access information about language resources relevant to your research."
+	},
+	{
+		href: 'http://www.clarin.eu/content/content-search',
+		image: 'https://www.clarin.eu/sites/default/files/search.png',
+		title: "Content Search (prototype version)",
+		description: "Search different corpora with a single search engine"
+	},
+	{
+		href: 'http://www.clarin.eu/contact',
+		image: 'https://www.clarin.eu/sites/default/files/questionmark.png',
+		title: "Consulting Services",
+		description: "Searching for a specific data set or application? Wondering how CLARIN can assist your research? Feel free to contact us!"
+	},
+];
+
 /**
  * This file is structured like this:
  * 1. our own code with function `main`
@@ -8,71 +67,6 @@
  * 3. inline bootstrap dropdown plugin, trimmed
  * 4. calling `main`
  */
-var more_img = "img/more-clarin-services.png";
-var service_list = [
-	[
-		{
-			href: 'http://www.clarin.eu/portal',
-			image: 'https://www.clarin.eu/sites/default/files/portal_small.png',
-			title: "CLARIN portal",
-			description: "Quick introduction to CLARIN, giving an impression about what's currently available",
-		},
-		{
-			href: 'http://www.clarin.eu/services/depositing',
-			image: 'http://www.clarin.eu/sites/default/files/long-term-archive.jpg',
-			title: "Depositing services",
-			description: "Store language resources in a sustainable repository at a CLARIN centre",
-		},
-		{
-			href: 'http://www.clarin.eu/vlo',
-			image: 'https://www.clarin.eu/sites/default/files/vlo_small.png',
-			title: "Virtual Language Observatory",
-			description: "Discover language resources using a faceted browser or a map",
-		},
-	],
-	[
-		{
-			href: 'http://www.clarin.eu/content/easy-access-protected-resources',
-			image: 'http://www.clarin.eu/sites/default/files/federated-identity.jpg',
-			title: "Easy federated access",
-			description: "Get easy access to protected resources, with your institutional username and password.",
-		},
-		{
-			href: 'http://www.clarin.eu/content/web-services',
-			image: 'https://www.clarin.eu/sites/default/files/webservice.png',
-			title: "Web services and applications",
-			description: "Explore and analyze language data with a wide variety of tools",
-		},
-		{
-			href: 'http://www.clarin.eu/content/virtual-collections',
-			image: 'https://www.clarin.eu/sites/default/files/virtualcollection.png',
-			title: "Virtual Collections",
-			description: "Create your own digital bookmarks, ideal for citing data sets.",
-		},
-	],
-	[
-		{
-			href: 'http://www.clarin.eu/content/language-resource-inventory',
-			image: 'https://www.clarin.eu/sites/default/files/lrt-inventory_small.png',
-			title: "Language Resource Inventory",
-			description: "Submit and access information about language resources relevant to your research."
-		},
-		{
-			href: 'http://www.clarin.eu/content/content-search',
-			image: 'https://www.clarin.eu/sites/default/files/search.png',
-			title: "Content Search (prototype version)",
-			description: "Search different corpora with a single search engine"
-		},
-		{
-			href: 'http://www.clarin.eu/contact',
-			image: 'https://www.clarin.eu/sites/default/files/questionmark.png',
-			title: "Consulting Services",
-			description: "Searching for a specific data set or application? Wondering how CLARIN can assist your research? Feel free to contact us!"
-		},
-	],
-];
-
-// TODO: now that we're using Zepto, replace manual html management with $ calls
 
 var entityMap = {
 	"&": "&amp;",
@@ -90,41 +84,40 @@ function escapeHtml(string) {
 }
 
 function renderCell(service) {
-	var href = escapeHtml(service.href);
-	var image = escapeHtml(service.image);
-	var title = escapeHtml(service.title);
-	var description = escapeHtml(service.description);
-
-	var p_image	=	"<img src='" + image + "'>";
-	var p_title	=	"<div>" + title + "</div>";
-
-	return "<td title='"+description+"'><a href='" + href + "'><div>" +
-				p_image + p_title +
-			"</div></a></td>";
-}
-
-function renderRow(services) {
-	return "<tr>" + services.map(renderCell).join('') + "</tr>";
+	var img = $('<img>').attr('src', service.image);
+	var title = $('<div>').text(escapeHtml(service.title));
+	var div = $('<div>').append(img).append(title);
+	var a = $('<a>').attr('href', service.href).append(div);
+	var li = $('<li>').attr('title', escapeHtml(service.description)).append(a);
+	console.log(li);
+	return li;
 }
 
 function renderDropdown(service_table) {
-	var rows = service_list.map(renderRow).join('');
-	var tablediv = "<div><table><tbody>" + rows + "</tbody></table></div>";
-	return '<div class="dropdown">' +
-		'<button class="btn dropdown-toggle" type="button"'+
-				' id="dropdown-clarinservices" data-toggle="dropdown" aria-expanded="true">'+
-			'<div id="clarinmore"><img src="'+more_img+'"></div> '+
-		'</button>'+
-		'<ul class="dropdown-menu" role="menu" aria-labelledby="dropdown-clarinservices">'+
-			tablediv +
-		'</ul>'+
-	'</div>';
+	var img = $('<img>').attr('src', more_img);
+	var imgdiv = $('<div>').attr('id', 'clarinmore').append(img);
+	var button = $('<button>').attr({
+		'class': 'btn dropdown-toggle',
+		type: 'button',
+		id:"dropdown-clarinservices",
+		'data-toggle': "dropdown",
+		'aria-expanded':"true"
+	}).append(imgdiv);
+
+	var ul = $('<ul>').attr({
+		'class': 'dropdown-menu',
+		role: 'menu',
+		"aria-labelledby": "dropdown-clarinservices"
+	});
+	service_list.map(renderCell).forEach(function(x){x.appendTo(ul);});
+
+	return $('<div>').attr('class', 'dropdown').append(button).append(ul);
 }
 
 function main() {
 	var csdiv = document.getElementById('clarinservices');
 	if (csdiv) {
-		csdiv.innerHTML = renderDropdown(service_list);
+		$(csdiv).append(renderDropdown(service_list));
 		$(csdiv).children().each(function (x) {
 			$(x).data('bs.dropdown', new Dropdown(x));
 		});
@@ -185,7 +178,6 @@ window.$ = Zepto;
   Dropdown.VERSION = '3.3.4'
 
   Dropdown.prototype.toggle = function (e) {
-  	console.log('tggle');
     var $this = $(this)
 
     if ($this.is('.disabled, :disabled')) return
@@ -251,7 +243,6 @@ window.$ = Zepto;
   }
 
   function clearMenus(e) {
-  	console.log('clearMenus', e);
     if (e && e.which === 3) return
     $(backdrop).remove()
     $(toggle).each(function () {
@@ -271,7 +262,6 @@ window.$ = Zepto;
   }
 
   function getParent($this) {
-  	console.log('getParent', $this);
     var selector = $this.attr('data-target')
 
     if (!selector) {
--- src/main/resources/assets/js/main.jsx
+++ src/main/resources/assets/js/main.jsx
@@ -2,7 +2,7 @@
 (function() {
 "use strict";
 
-var VERSION = window.MyAggregator.VERSION = "v.2.0.0-beta-45";
+var VERSION = window.MyAggregator.VERSION = "v.2.0.0-beta-46";
 
 var URLROOT = window.MyAggregator.URLROOT =
 	window.location.pathname.substring(0, window.location.pathname.indexOf("/",2)) ||
