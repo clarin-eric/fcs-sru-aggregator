c709c242d955af12d7f20943102b716f1992299f
Leif-Jöran Olsson <ljo@fripost.org>
2019-11-11 17:01:54 +0100

    Adding https to all URLs in clarinservices js-resource to avoid mexed content warnings.

--- src/main/resources/assets/clarinservices/clarinservices.js
+++ src/main/resources/assets/clarinservices/clarinservices.js
@@ -3,55 +3,55 @@
 
 var service_list = [
 	{
-		href: 'http://www.clarin.eu/portal',
+		href: 'https://www.clarin.eu/portal',
 		image: 'https://www.clarin.eu/sites/default/files/portal_small.png',
 		title: "CLARIN portal",
 		description: "Quick introduction to CLARIN, giving an impression about what's currently available",
 	},
 	{
-		href: 'http://www.clarin.eu/services/depositing',
-		image: 'http://www.clarin.eu/sites/default/files/long-term-archive.jpg',
+		href: 'https://www.clarin.eu/services/depositing',
+		image: 'https://www.clarin.eu/sites/default/files/long-term-archive.jpg',
 		title: "Depositing services",
 		description: "Store language resources in a sustainable repository at a CLARIN centre",
 	},
 	{
-		href: 'http://www.clarin.eu/vlo',
+		href: 'https://www.clarin.eu/vlo',
 		image: 'https://www.clarin.eu/sites/default/files/vlo_small.png',
 		title: "Virtual Language Observatory",
 		description: "Discover language resources using a faceted browser or a map",
 	},
 	{
-		href: 'http://www.clarin.eu/content/easy-access-protected-resources',
-		image: 'http://www.clarin.eu/sites/default/files/federated-identity.jpg',
+		href: 'https://www.clarin.eu/content/easy-access-protected-resources',
+		image: 'https://www.clarin.eu/sites/default/files/federated-identity.jpg',
 		title: "Easy federated access",
 		description: "Get easy access to protected resources, with your institutional username and password.",
 	},
 	{
-		href: 'http://www.clarin.eu/content/web-services',
+		href: 'https://www.clarin.eu/content/web-services',
 		image: 'https://www.clarin.eu/sites/default/files/webservice.png',
 		title: "Web services and applications",
 		description: "Explore and analyze language data with a wide variety of tools",
 	},
 	{
-		href: 'http://www.clarin.eu/content/virtual-collections',
+		href: 'https://www.clarin.eu/content/virtual-collections',
 		image: 'https://www.clarin.eu/sites/default/files/virtualcollection.png',
 		title: "Virtual Collections",
 		description: "Create your own digital bookmarks, ideal for citing data sets.",
 	},
 	{
-		href: 'http://www.clarin.eu/content/language-resource-inventory',
+		href: 'https://www.clarin.eu/content/language-resource-inventory',
 		image: 'https://www.clarin.eu/sites/default/files/lrt-inventory_small.png',
 		title: "Language Resource Inventory",
 		description: "Submit and access information about language resources relevant to your research."
 	},
 	{
-		href: 'http://www.clarin.eu/content/content-search',
+		href: 'https://www.clarin.eu/content/content-search',
 		image: 'https://www.clarin.eu/sites/default/files/search.png',
 		title: "Federated Content Search",
 		description: "Search different corpora with a single search engine"
 	},
 	{
-		href: 'http://www.clarin.eu/contact',
+		href: 'https://www.clarin.eu/contact',
 		image: 'https://www.clarin.eu/sites/default/files/questionmark.png',
 		title: "Consulting Services",
 		description: "Searching for a specific data set or application? Wondering how CLARIN can assist your research? Feel free to contact us!"
