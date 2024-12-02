3f471fa894b4a054f042ee861be46c21970c3158
Erik Körner <koerner@saw-leipzig.de>
2024-12-02 19:17:13 +0100

    better key config

--- src/main/java/eu/clarin/sru/fcs/aggregator/app/AggregatorConfiguration.java
+++ src/main/java/eu/clarin/sru/fcs/aggregator/app/AggregatorConfiguration.java
@@ -3,9 +3,16 @@ package eu.clarin.sru.fcs.aggregator.app;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import io.dropwizard.core.Configuration;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
@@ -303,6 +310,7 @@ public class AggregatorConfiguration extends Configuration {
 
             public static class KeyConfig {
                 @JsonProperty
+                @JsonDeserialize(using = PEMKeyStringDeserializer.class)
                 String publicKey;
 
                 @JsonIgnore
@@ -310,7 +318,10 @@ public class AggregatorConfiguration extends Configuration {
                     return publicKey;
                 }
 
+                // TODO: better masking with
+                // https://stackoverflow.com/questions/56070451/mask-json-fields-using-jackson
                 @JsonProperty(access = Access.WRITE_ONLY)
+                @JsonDeserialize(using = PEMKeyStringDeserializer.class)
                 String privateKey;
 
                 @JsonIgnore
@@ -320,6 +331,67 @@ public class AggregatorConfiguration extends Configuration {
 
                 // TODO: maybe key path properties?
                 // TODO: validation?
+
+                /**
+                 * Helper to correctly deserialize PEM keys when e.g., supplied via environment
+                 * variable substitution which doesn't correctly preserve the line breaks.
+                 */
+                public static class PEMKeyStringDeserializer extends JsonDeserializer<String> {
+                    private final static String MARKER_DELIMS = "-----";
+                    private final static int MARKER_DELIMS_LEN = MARKER_DELIMS.length();
+
+                    @Override
+                    public String deserialize(JsonParser p, DeserializationContext ctxt)
+                            throws IOException, JacksonException {
+                        String contents = p.getText().strip();
+                        if (!contents.startsWith(MARKER_DELIMS) || !contents.endsWith(MARKER_DELIMS)) {
+                            throw new JsonParseException(p, "Expected PEM key block with '-----' delimiters!");
+                        }
+                        if (contents.indexOf('\n') != -1) {
+                            // has line breaks, we do nothing
+                            return contents;
+                        }
+                        // no line breaks
+                        if (contents.indexOf("\\n") != -1) {
+                            contents = contents.replace("\\n", "\n");
+                            return contents;
+                        }
+                        // probably line breaks converted to single spaces
+                        int posStartOfStartBlock = contents.indexOf(MARKER_DELIMS + "BEGIN ");
+                        if (posStartOfStartBlock == -1) {
+                            throw new JsonParseException(p,
+                                    "Unexpected PEM structure, expected '-----BEGIN ' start of BEGIN block indicator!");
+                        }
+                        int posEndOfStartBlock = contents.indexOf(MARKER_DELIMS,
+                                posStartOfStartBlock + MARKER_DELIMS_LEN + 6 + 1);
+                        if (posEndOfStartBlock == -1) {
+                            throw new JsonParseException(p,
+                                    "Unexpected PEM structure, expected '----- ' end of BEGIN block indicator!");
+                        }
+
+                        int posStartOfEndBlock = contents.indexOf(MARKER_DELIMS + "END ",
+                                posEndOfStartBlock + MARKER_DELIMS_LEN + 1);
+                        if (posStartOfEndBlock == -1) {
+                            throw new JsonParseException(p,
+                                    "Unexpected PEM structure, expected '-----END ' start of END block indicator!");
+                        }
+                        int posEndOfEndBlock = contents.indexOf(MARKER_DELIMS,
+                                posStartOfEndBlock + MARKER_DELIMS_LEN + 4 + 1);
+                        if (posEndOfEndBlock == -1) {
+                            throw new JsonParseException(p,
+                                    "Unexpected PEM structure, expected '-----' end of END block indicator!");
+                        }
+
+                        int posStartContents = posEndOfStartBlock + MARKER_DELIMS_LEN;
+                        int posEndContents = posStartOfEndBlock;
+
+                        String header = contents.substring(0, posEndOfStartBlock + MARKER_DELIMS_LEN);
+                        String footer = contents.substring(posStartOfEndBlock);
+                        String blockContents = contents.substring(posStartContents, posEndContents).strip().replace(' ',
+                                '\n');
+                        return header + "\n" + blockContents + "\n" + footer;
+                    }
+                }
             }
 
             @Valid
