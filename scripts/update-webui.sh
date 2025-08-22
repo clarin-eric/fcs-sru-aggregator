ae966c3ab21776f0e74c6d2ec211307ff7ed4df7
Erik Körner <koerner@saw-leipzig.de>
2025-08-22 18:26:56 +0200

    Update webui

--- scripts/update-webui.sh
+++ scripts/update-webui.sh
@@ -37,6 +37,34 @@ FN_CUSTOMIZATION_ENV=aggregator-webui/.env
 
 # --------------------------------------------------------------------------
 
+# inject some maven project info
+
+APPL_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+# short commit hash
+GIT_SHORT_SHA=$(git rev-parse --short HEAD)
+# commit date
+GIT_DATE=$(git log -1 --pretty="format:%cI")
+# short branch name
+GIT_SHORT_REF=$(git branch --show-current)
+#GIT_SHORT_REF=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+# tag (at HEAD)
+GIT_TAG=$(git tag --points-at HEAD)
+# last tag in current branch
+GIT_LAST_TAG=$(git describe --tags --abbrev=0 2>/dev/null)
+
+
+touch ${FN_CUSTOMIZATION_ENV}
+sed -i '/^VITE_APPLICATION_VERSION/d' ${FN_CUSTOMIZATION_ENV}
+echo "VITE_APPLICATION_VERSION=${APPL_VERSION}" >> ${FN_CUSTOMIZATION_ENV}
+sed -i '/^VITE_GIT_APP/d' ${FN_CUSTOMIZATION_ENV}
+echo "VITE_GIT_APP_INFO_SHA=${GIT_SHORT_SHA}" >> ${FN_CUSTOMIZATION_ENV}
+echo "VITE_GIT_APP_INFO_REF=${GIT_SHORT_REF}" >> ${FN_CUSTOMIZATION_ENV}
+echo "VITE_GIT_APP_INFO_TAG=${GIT_TAG}" >> ${FN_CUSTOMIZATION_ENV}
+echo "VITE_GIT_APP_INFO_DATE=${GIT_DATE}" >> ${FN_CUSTOMIZATION_ENV}
+
+# --------------------------------------------------------------------------
+
 pushd aggregator-webui
 
 # build webui
